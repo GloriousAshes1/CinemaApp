6207f53b6c9b08edc98afb8d000faf837e9d5f27
GloriousAshes1 <duyloc03@gmail.com>
2024-12-15 15:26:44 +0700

    Update Category aCtivity

--- app/src/main/java/com/example/finalprojectmobileapplication/activity/CategoryActivity.java
+++ app/src/main/java/com/example/finalprojectmobileapplication/activity/CategoryActivity.java
@@ -3,24 +3,84 @@ package com.example.finalprojectmobileapplication.activity;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
 
+import com.example.finalprojectmobileapplication.MyApplication;
 import com.example.finalprojectmobileapplication.R;
+import com.example.finalprojectmobileapplication.adapter.MovieAdapter;
+import com.example.finalprojectmobileapplication.constant.ConstantKey;
+import com.example.finalprojectmobileapplication.constant.GlobalFunction;
+import com.example.finalprojectmobileapplication.databinding.ActivityCategoryBinding;
+import com.example.finalprojectmobileapplication.model.Category;
+import com.example.finalprojectmobileapplication.model.Movie;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CategoryActivity extends AppCompatActivity {
+
+    private ActivityCategoryBinding mActivityCategoryBinding;
+    private List<Movie> mListMovies;
+    private Category mCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_category);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        mActivityCategoryBinding = ActivityCategoryBinding.inflate(getLayoutInflater());
+        setContentView(mActivityCategoryBinding.getRoot());
+
+        getDataIntent();
+        initListener();
+        getListMovies();
+    }
+    private void getDataIntent() {
+        Bundle bundleReceived = getIntent().getExtras();
+        if (bundleReceived != null) {
+            mCategory = (Category) bundleReceived.get(ConstantKey.KEY_INTENT_CATEGORY_OBJECT);
+            mActivityCategoryBinding.tvTitle.setText(mCategory.getName());
+        }
+    }
+
+    private void initListener() {
+        mActivityCategoryBinding.imgBack.setOnClickListener(v -> onBackPressed());
+    }
+
+    private void getListMovies() {
+        MyApplication.get(this).getMovieDatabaseReference().addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (mListMovies != null) {
+                    mListMovies.clear();
+                } else {
+                    mListMovies = new ArrayList<>();
+                }
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Movie movie = dataSnapshot.getValue(Movie.class);
+                    if (movie != null && mCategory.getId() == movie.getCategoryId()) {
+                        mListMovies.add(0, movie);
+                    }
+                }
+                displayListMovies();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
         });
+    }
+
+    private void displayListMovies() {
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
+        mActivityCategoryBinding.rcvData.setLayoutManager(gridLayoutManager);
+        MovieAdapter movieAdapter = new MovieAdapter(mListMovies,
+                movie -> GlobalFunction.goToMovieDetail(this, movie));
+        mActivityCategoryBinding.rcvData.setAdapter(movieAdapter);
     }
 }
