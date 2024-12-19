4881fa1c9a63cf9e8ed94181ce1acff3e5d48dde
duy3011 <vokhacdiuy@gmail.com>
2024-12-19 11:02:45 +0700

    fix revenue + ticket

--- app/src/main/java/com/example/finalprojectmobileapplication/fragment/admin/AdminManageFragment.java
+++ app/src/main/java/com/example/finalprojectmobileapplication/fragment/admin/AdminManageFragment.java
@@ -5,19 +5,49 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.finalprojectmobileapplication.activity.ChangePasswordActivity;
+import com.example.finalprojectmobileapplication.activity.SignInActivity;
+import com.example.finalprojectmobileapplication.activity.admin.AdminRevenueActivity;
+import com.example.finalprojectmobileapplication.constant.GlobalFunction;
 import com.example.finalprojectmobileapplication.databinding.FragmentAdminManageBinding;
+import com.example.finalprojectmobileapplication.prefs.DataStoreManager;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AdminManageFragment extends Fragment {
 
-    private FragmentAdminManageBinding fragmentAdminManageBinding;
-
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        fragmentAdminManageBinding = FragmentAdminManageBinding.inflate(inflater, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        FragmentAdminManageBinding fragmentAdminManageBinding = FragmentAdminManageBinding.inflate(inflater, container, false);
+
+        fragmentAdminManageBinding.tvEmail.setText(DataStoreManager.getUser().getEmail());
+
+        fragmentAdminManageBinding.layoutReport.setOnClickListener(v -> onClickReport());
+        fragmentAdminManageBinding.layoutSignOut.setOnClickListener(v -> onClickSignOut());
+        fragmentAdminManageBinding.layoutChangePassword.setOnClickListener(v -> onClickChangePassword());
 
         return fragmentAdminManageBinding.getRoot();
+    }
+
+    private void onClickReport() {
+        GlobalFunction.startActivity(getActivity(), AdminRevenueActivity.class);
+    }
+
+    private void onClickChangePassword() {
+        GlobalFunction.startActivity(getActivity(), ChangePasswordActivity.class);
+    }
+
+    private void onClickSignOut() {
+        if (getActivity() == null) {
+            return;
+        }
+        FirebaseAuth.getInstance().signOut();
+        DataStoreManager.setUser(null);
+        GlobalFunction.startActivity(getActivity(), SignInActivity.class);
+        getActivity().finishAffinity();
     }
 }
